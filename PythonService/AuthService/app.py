13e605e57ae22156925d9b124fca94135fe8f28e
lexibrown <lexibrown@cmail.carleton.ca>
2018-03-05 16:02:12 -0500

    auth changes

--- PythonService/AuthService/app.py
+++ PythonService/AuthService/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, abort, jsonify, Response, json
 from Crypto.Cipher import XOR
-import base64, time, redis
+import base64, time, redis, datetime
+import logging, sys
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 app = Flask(__name__)
 
@@ -8,7 +11,7 @@ cache = redis.Redis(host='redis', port=6379)
 
 delimiter = '\\';
 key = "this is the secret key"
-import datetime
+
 def encrypt(plaintext):
 	cipher = XOR.new(key)
 	return base64.b64encode(cipher.encrypt(plaintext))
@@ -28,11 +31,22 @@ def invalid(error=None):
 	resp.status_code = 400
 	return resp
 	
+@app.errorhandler(401)
+def unauthorized(error=None):
+	message = {
+			'status': 401,
+			'error': 'AUTH-1001',
+			'message': 'Token is invalid'
+	}
+	resp = jsonify(message)
+	resp.status_code = 401
+	return resp
+	
 @app.errorhandler(408)
 def timeout(error=None):
 	message = {
 			'status': 408,
-			'error': 'AUTH-2000',
+			'error': 'AUTH-1008',
 			'message': 'Token has expired'
 	}
 	resp = jsonify(message)
@@ -62,9 +76,27 @@ def login():
 
 	resp = Response(js, status = 200, mimetype = 'application/json')
 	return resp
+	
+@app.route('/auth/logout', methods = ['POST'])
+def logout():
+	if not request.json:
+		abort(400)
+	content = request.get_json()
 
-import logging, sys
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+	token = decrypt(content['token']).decode('utf-8').split('\\')
+	
+	if cache.get(token[0]) is None:
+		abort(401)
+	
+	cache.delete(token[0])
+	
+	data = {
+		'message' : 'Logged out'
+	}
+	js = json.dumps(data)
+
+	resp = Response(js, status = 200, mimetype = 'application/json')
+	return resp
 
 @app.route('/auth/verify', methods = ['POST'])
 def verify():
@@ -75,7 +107,7 @@ def verify():
 	token = decrypt(content['token']).decode('utf-8').split('\\')
 	
 	if cache.get(token[0]) is None:
-		abort(400)
+		abort(401)
 
 	then = float(token[2])
 	now = time.time()
@@ -86,12 +118,10 @@ def verify():
 	st = datetime.datetime.fromtimestamp(then).strftime('%Y-%m-%d %H:%M:%S')
 	logging.debug("Then: " + st)
 	logging.debug("Diff: " + str(elapsed))
-	
-	#m, s = divmod(elapsed, 60)
-	#h, m = divmod(m, 60)
+
 	m = int(elapsed) / 60
 	
-	if m > 1:
+	if m > 10:
 		abort(408)
 	
 	data = {
@@ -107,8 +137,6 @@ def user():
 	if not request.json:
 		abort(400)
 	content = request.get_json()
-	print (content)
-	print (content['token'])
 
 	token = decrypt(content['token'])
 	
