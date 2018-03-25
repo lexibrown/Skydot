1358b15a661ce73ca2fc4e2660fcea30f3c8a4a3
lexibrown <lexibrown@cmail.carleton.ca>
2018-03-25 12:36:23 -0400

    more python services

---
+++ PythonService/VerifyService/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, abort, jsonify, Response, json
+import logging, sys, requests
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+app = Flask(__name__)
+
+@app.errorhandler(400)
+def invalid(error=None):
+	message = {
+			'status': 400,
+			'error': 'AUTH-1000',
+			'message': 'Invalid request'
+	}
+	resp = jsonify(message)
+	resp.status_code = 400
+	return resp
+
+@app.route('/auth', methods = ['POST'])
+def verify():
+	if not request.json:
+		abort(400)
+	content = request.get_json()
+
+	if 'user_id' not in content:
+		abort(400)
+		
+	if 'password' not in content:
+		abort(400)
+		
+	url = 'https://skydot-bank.azurewebsites.net/host/verify'
+	
+	logging.debug(url)
+	
+	response = requests.post(url = url, json = content)
+	js = json.dumps(response.json())
+
+	logging.debug(js)
+	
+	if 'error' in js:
+		return Response(js, status = 401, mimetype = 'application/json')
+	
+	return Response(js, status = 200, mimetype = 'application/json')
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", debug=True)
