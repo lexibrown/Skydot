d2b793949b426cb667dc1a46c4600b5b33419013
lexibrown <lexibrown@cmail.carleton.ca>
2018-04-11 02:50:10 -0400

    javascript logging and error handling

--- JavaScriptService/BillPayeeService/server.js
+++ JavaScriptService/BillPayeeService/server.js
@@ -14,54 +14,93 @@ var options = {
     }
 };
 
+var invalid = {
+	error: 'BILL-1000',
+	message: 'Invalid request'
+};
+
+var crash = {
+	error: 'BILL-5000',
+	message: 'Something went wrong. Please try again.'
+};
+
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/bill/payee/search', function(req, res){
-    console.log(req.body);
+var moment = require('moment'),
+timestamp = moment().format('YYYY-MM-DD HH:mm:ss.SSS');
 
-    var params = req.body;
-	
-	if (!(params.search)) {
-		console.log("invalid params")
-		res.status(400).json({
-			error: 'BILL-1000',
-			message: 'Invalid request'
-		});
-	} else {
-		request.post({
+var morgan = require('morgan')
+morgan.token("date-time", function (req, res) { return timestamp });
+app.use(morgan(':date-time [:method] :url :status - :response-time ms'))
+
+function handleError(res, error) {
+	console.log ('error', error.message, error.stack)
+	console.log("[RESPONSE] ", crash);
+	res.status(500).json(crash);
+};
+
+app.post('/bill/payee/search', function(req, res){
+	try {
+		console.log("[REQUEST] ", req.body);
+
+		var params = req.body;
+		
+		if (!(params.search)) {
+			console.log("invalid params");
+			console.log("[RESPONSE] ", invalid);
+			res.status(400).json(invalid);
+		} else {
+			request.post({
+				url: "http://host-gateway/host/bill/payee",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: params,
+				json:true
+			}, function(error, response, body){
+				if (error) {
+					handleError(res, error);
+					return;
+				}
+				console.log("[RESPONSE] ", body);
+				if (body.error) {
+					res.status(400).json(body);
+					return;
+				}
+				res.send(body);					
+			});
+		}
+	} catch (error) {
+		handleError(res, error);
+	}
+});
+
+app.post('/bill/payee', function(req, res){
+	try {
+		console.log("[REQUEST] ", req.body);
+
+		request.get({
 			url: "http://host-gateway/host/bill/payee",
 			headers: {
 				"Content-Type": "application/json"
 			},
-			body: params,
-			json:true
 		}, function(error, response, body){
+			if (error) {
+				handleError(res, error);
+				return;
+			}
+			console.log("[RESPONSE] ", body);
 			if (body.error) {
 				res.status(400).json(body);
 				return;
 			}
+			res.setHeader('Content-Type', 'application/json');
 			res.send(body);
 		});
-	}	
-});
-
-app.post('/bill/payee', function(req, res){
-    console.log(req.body);
-
-	request.get({
-		url: "http://host-gateway/host/bill/payee",
-		headers: {
-			"Content-Type": "application/json"
-		},
-	}, function(error, response, body){
-		if (body.error) {
-			res.status(400).json(body);
-			return;
-		}
-		res.setHeader('Content-Type', 'application/json');
-		res.send(body);
-	});
+	} catch (error) {
+		handleError(res, error);
+	}
 });
 
 app.listen(PORT, HOST);
--- JavaScriptService/BillService/server.js
+++ JavaScriptService/BillService/server.js
@@ -14,36 +14,66 @@ var options = {
     }
 };
 
+var invalid = {
+	error: 'BILL-1000',
+	message: 'Invalid request'
+};
+
+var crash = {
+	error: 'BILL-5000',
+	message: 'Something went wrong. Please try again.'
+};
+
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/bill', function(req, res){
-    console.log(req.body);
+var moment = require('moment'),
+timestamp = moment().format('YYYY-MM-DD HH:mm:ss.SSS');
 
-    var params = req.body;
-	
-	if (!(params.user_id) || !(params.from_account) || !(params.payee) || !(params.amount) || !(params.currency)) {
-		console.log("invalid params")
-		res.status(400).json({
-			error: 'BILL-1000',
-			message: 'Invalid request'
-		});
-	} else {
-		request.post({
-			url: "http://host-gateway/host/bill",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: params,
-			json:true
-		}, function(error, response, body){
-			if (body.error) {
-				res.status(400).json(body);
-				return;
-			}
-			res.send(body);
-		});
-	}	
+var morgan = require('morgan')
+morgan.token("date-time", function (req, res) { return timestamp });
+app.use(morgan(':date-time [:method] :url :status - :response-time ms'))
+
+function handleError(res, error) {
+	console.log ('error', error.message, error.stack)
+	console.log("[RESPONSE] ", crash);
+	res.status(500).json(crash);
+};
+
+app.post('/bill', function(req, res){
+    try {
+		console.log("[REQUEST] ", req.body);
+
+		var params = req.body;
+		
+		if (!(params.user_id) || !(params.from_account) || !(params.payee) || !(params.amount) || !(params.currency)) {
+			console.log("invalid params")
+			console.log("[RESPONSE] ", invalid);
+			res.status(400).json(invalid);
+		} else {
+			request.post({
+				url: "http://host-gateway/host/bill",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: params,
+				json:true
+			}, function(error, response, body){
+				if (error) {
+					handleError(res, error);
+					return;
+				}
+				console.log("[RESPONSE] ", body);
+				if (body.error) {
+					res.status(400).json(body);
+					return;
+				}
+				res.send(body);
+			});
+		}
+	} catch (error) {
+		handleError(res, error);
+	}
 });
 
 app.listen(PORT, HOST);
